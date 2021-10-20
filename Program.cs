77c5af3b627b4ff0c0ab5aa5a9e9796f3c54e459
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-10-20 19:59:18 +0300

    c# 101 2. ders örnekler

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String str1 = string.Empty;
+            str1 = "yavuz kocar";
+            string ad = "yavuz";
+            string soyad = "kocar";
+            string tamisim = ad + " " + soyad;
+            Console.WriteLine(tamisim);
+
+        }
+    }
+}
